e3a7d4a51ae025120a9b45b72822ab193a0af79d
domiinek <dkaczmarska97@gmail.com>
2020-04-06 21:49:13 +0200

    blablabla

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from fastapi import FastAPI, Request
 
 app = FastAPI()
 
+@app.get('/')
+def hello_world():
+    return {"message": "Hello World during the coronavirus pandemic!"}
 
 
-
-@app.get("/items/{item_id}")
-def read_root(item_id: str, request: Request):
-    client_host = request.client.host
-    return {"client_host": client_host, "item_id": item_id}
+@app.api_route(path="/method", method=["GET", "POST", "PUT", "DELETE"])
+def requests():
+	return{"method": requests.method}
