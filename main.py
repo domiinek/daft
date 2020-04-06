619ea046a07a7cd4bc3111ee9e8070a8f01ccb9c
domiinek <dkaczmarska97@gmail.com>
2020-04-06 21:38:58 +0200

    dziwna wersja

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ app = FastAPI()
 def hello_world():
     return {"message": "Hello World during the coronavirus pandemic!"}
 
-@app.api_route(path="/method", methods=["GET", "POST", "PUT","DELETE"])
-def our_requests(request: Request):
-    return{"method": request.methods}
+@app.get("/items/{item_id}")
+def read_root(item_id: str, request: Request):
+    client_host = request.client.host
+    return {"client_host": client_host, "item_id": item_id}
