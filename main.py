15b48db7487f450dbf587d082f3a71e1d27b6145
domiinek <dkaczmarska97@gmail.com>
2020-04-06 21:03:29 +0200

    zadanie2

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get('/')
 def hello_world():
     return {"message": "Hello World during the coronavirus pandemic!"}
+
+@app.api_route(path="/method", methods=["GET", "POST", "DELETE", "PUT", "OPTIONS"])
+def our_requests(request: Request):
+    return{"method": request.method}
