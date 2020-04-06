e987bbb0b1d6816bb918173fd11c2c328b593756
domiinek <dkaczmarska97@gmail.com>
2020-04-06 21:41:37 +0200

    without zad1

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ from fastapi import FastAPI, Request
 app = FastAPI()
 
 
-@app.get('/')
-def hello_world():
-    return {"message": "Hello World during the coronavirus pandemic!"}
+
 
 @app.get("/items/{item_id}")
 def read_root(item_id: str, request: Request):
