310bf7215ea423468ad315e1f7b243684a5b9ee5
Dominika <42723145+domiinek@users.noreply.github.com>
2020-04-27 01:27:18 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -5,13 +5,23 @@ from fastapi import Depends, Cookie, HTTPException
 from hashlib import sha256
 
 import secrets
+from typing import Dict
+
+from fastapi import FastAPI, Request, Response, status
+from pydantic import BaseModel
+
+
+class Patient(BaseModel):
+    name: str
+    surename: str
 
 
 app = FastAPI()
 security = HTTPBasic()
 app.session_tokens = []
 app.secret_key = "my secret key"
-
+app.counter: int = 0
+app.storage: Dict[int, Patient] = {}
 
 
 @app.get("/")
@@ -19,8 +29,12 @@ def read_root():
     return {"message": "Hello World during the coronavirus pandemic!"}
 
 @app.get("/welcome")
-def welcoming():
-    return {"message": "Some stupid message"}
+def welcome(request: Request, session_token: str =Cookie(None)):
+	if session_token in app.sesions:
+		return templates.TemplateResponse("welcome.html", {"request":request, "user" : app.sesions[session_token]})
+	else:
+		return HTTPException(401)
+
 
 @app.post("/login")
 def login_auth(response: Response, credentials: HTTPBasicCredentials = Depends(security)):
@@ -34,3 +48,24 @@ def login_auth(response: Response, credentials: HTTPBasicCredentials = Depends(s
     response.set_cookie(key="session_token", value=session_token)
     response.headers["Location"] = "/welcome"
     response.status_code = status.HTTP_302_FOUND
+
+
+
+@app.api_route(path="/method", methods=["GET", "POST", "DELETE", "PUT", "OPTIONS"])
+def read_request(request: Request):
+    return {"method": request.method}
+
+
+@app.post("/patient")
+def show_data(patient: Patient):
+    resp = {"id": app.counter, "patient": patient}
+    app.storage[app.counter] = patient
+    app.counter += 1
+    return resp
+
+
+@app.get("/patient/{pk}")
+def show_patient(pk: int):
+    if pk in app.storage:
+        return app.storage.get(pk)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
