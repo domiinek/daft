83d9d40f8ec36b175e087ae26161d7f9843163fa
Dominika <42723145+domiinek@users.noreply.github.com>
2020-04-26 22:09:52 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
-from fastapi import FastAPI, Request
+from typing import Dict
+
+from fastapi import FastAPI, Request, Response, status
+from pydantic import BaseModel
+
+
+class Patient(BaseModel):
+    name: str
+    surename: str
+
 
 app = FastAPI()
+app.counter: int = 0
+app.storage: Dict[int, Patient] = {}
 
-@app.get('/')
-def hello_world():
+
+@app.get("/")
+def read_root():
     return {"message": "Hello World during the coronavirus pandemic!"}
 
 
-@app.get(path="/method", method=["GET", "POST", "PUT", "DELETE"])
-def write_requests(requests:Request):
-	return{"method": requests.method}
+@app.api_route(path="/method", methods=["GET", "POST", "DELETE", "PUT", "OPTIONS"])
+def read_request(request: Request):
+    return {"method": request.method}
+
+
+@app.post("/patient")
+def show_data(patient: Patient):
+    resp = {"id": app.counter, "patient": patient}
+    app.storage[app.counter] = patient
+    app.counter += 1
+    return resp
+
+
+@app.get("/patient/{pk}")
+def show_patient(pk: int):
+    if pk in app.storage:
+        return app.storage.get(pk)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
