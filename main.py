0dbce3e782171134f6d6a635d3337ea0a53025c8
Dominika <42723145+domiinek@users.noreply.github.com>
2020-04-27 06:49:22 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def read_root():
     return {"message": "Hello World during the coronavirus pandemic!"}
 
 @app.get("/welcome")
-def welcome(request: Request, session_token: str = set_cookie(None)):
+def welcome(request: Request, session_token: str = Cookie(None)):
     if session_token not in app.session_tokens:
         return Response(status_code=status.HTTP_401_UNAUTHORIZED)
     else:
